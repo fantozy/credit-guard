70e5048abaa00e6012e2ad1d19d4689699f71710
Nika <FanTozy.v2@gmail.com>
2024-06-02 15:54:34 +0400

    Improved tests (Added random data pregenration)

--- credit_guard/cards/tests.py
+++ credit_guard/cards/tests.py
@@ -4,12 +4,26 @@ import random
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 from faker import Faker
-from .models import Card
 
 
 class CardTests(APITestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cls.user = User.objects.create_user(username="testuser", password="12345")
+
+        cls.faker = Faker()
+
+        cls.data_to_send = []
+
+        for _ in range(100):
+            data = {
+                "card_number": cls.faker.credit_card_number("visa16"),
+                "ccv": random.randint(100, 999),
+            }
+            cls.data_to_send.append(data)
+
     def setUp(self) -> None:
-        self.user = User.objects.create_user(username="testuser", password="12345")
         self.client.login(username="testuser", password="12345")
 
     def test_create_card(self):
@@ -22,18 +36,12 @@ class CardTests(APITestCase):
     def test_speed(self):
         start_time = time.time()
         api_url = "/api/v1/cards/"
-        faker = Faker()
 
-        for _ in range(100):
-
-            data = {
-                "card_number": faker.credit_card_number("visa16"),
-                "ccv": random.randint(100, 999),
-            }
-
-            response = self.client.post(api_url, data, format="json")
+        for i in self.data_to_send:
+            response = self.client.post(api_url, i, format="json")
             print(response.data)
             self.assertEqual(response.status_code, 201)
 
         end_time = time.time()
-        self.assertLess(end_time - start_time, 60)
+        print("100 request finished in: ", end_time - start_time, "seconds")
+        self.assertLess(end_time - start_time, 5)
